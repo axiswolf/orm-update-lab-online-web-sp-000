1c0e0be137dba4866aeb4e1c6a44b096c2f47c1a
Karen V <karenvo@usc.edu>
2019-08-21 03:35:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,7 +43,7 @@ class Student
     end
   end
 
-  def self.create(name:, grade:)
+  def self.create(name, grade)
     # creates a student with two attributes, name, and grade and saves it into the student's table.
     student = self.new(nil, name, grade)
     student.save
