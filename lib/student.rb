630cead993c877b70477b7c73c9dd86706b65bd0
Karen V <karenvo@usc.edu>
2019-08-21 03:33:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,9 +53,9 @@ class Student
   def self.new_from_db(row)
     # creates an instance with corresponding attribute values
     new_student = self.new(row[0], row[1], row[2])
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
+    # new_student.id = row[0]
+    # new_student.name = row[1]
+    # new_student.grade = row[2]
     new_student
   end
 
